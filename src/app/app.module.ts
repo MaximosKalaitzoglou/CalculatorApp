69a14dbe17cf187060161daffdad3bae13ebd154
Maximos <maximoskalai@gmail.com>
2023-09-20 19:52:55 +0300

    Main calculator body added plus some css styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule
