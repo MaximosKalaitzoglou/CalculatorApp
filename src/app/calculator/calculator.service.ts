4bb6bc8e1ff97bd300b4eeccc02c88f8c3a6637e
Maximos <maximoskalai@gmail.com>
2023-09-21 17:28:41 +0300

    Added digit state functionality

--- src/app/calculator/calculator.service.ts
+++ src/app/calculator/calculator.service.ts
@@ -4,10 +4,19 @@ const operators = ['+', '-', '×', '÷'];
 const functions = ['Log', 'ln', 'sin', 'cos', '!', 'tan', '√', '%'];
 
 export class CalculatorService {
+  /* display is updated by user input and returned to update the
+  current display
+
+  parenthesis is used to count the number of open parenthesis (
+
+  state is used to know the current input state
+
+  result is the output of the calculator when the "=" sign is pressed
+
+  */
   display: string = '0';
   parenthesis: number = 0;
   state: string = 'digit';
-  invalid: boolean = false;
   result: number = 0;
 
   displayWasUpdated = new EventEmitter<void>();
@@ -22,12 +31,6 @@ export class CalculatorService {
 
   isFunction(token: string): boolean {
     return functions.includes(token);
-  }
-
-  updateDisplay(value: string) {
-    this.determineStates(value);
-    console.log(this.state);
-    this.displayWasUpdated.emit();
   }
 
   determineStates(value: string) {
@@ -51,6 +54,43 @@ export class CalculatorService {
       //   return;
       // }
       this.state = 'digit';
+    }
+  }
+
+  updateDisplay(value: string) {
+    console.log(value);
+    const prevState = this.state;
+    this.determineStates(value);
+
+    switch (this.state) {
+      case 'operator':
+        break;
+      case 'digit':
+        this.digitState(value);
+        break;
+      case 'function':
+        break;
+      case 'CE':
+        break;
+      case 'C':
+        this.display = '0';
+        break;
+      case 'equals':
+        break;
+      case 'parenthesis-open':
+        break;
+      case 'parenthesis-close':
+        break;
+    }
+
+    this.displayWasUpdated.emit();
+  }
+
+  digitState(value: string) {
+    if (this.display.length < 2 && this.display[0] === '0') {
+      this.display = value;
+    } else {
+      this.display += value;
     }
   }
 }
